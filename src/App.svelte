d7ce4aa6871e34617fa6e1e1d71889b984d85481
Aleš Zima <zima.ales@seznam.cz>
2025-09-17 20:39:11 +0200

    added basic Counter layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+  import Counter from './lib/Counter.svelte'
+
+  const playerLives = $state([20, 20])
+
+  function incrementLife(playerIndex: number) {
+    playerLives[playerIndex] += 1
+  }
+
+  function decrementLife(playerIndex: number) {
+    playerLives[playerIndex] -= 1
+  }
 </script>
 
-<main class="max-w-sm text-center mx-auto">
-  <h1>Title</h1>
+<main class="max-w-sm text-center mx-auto h-lvh grid">
+  <div class="rotate-180 grid">
+    <Counter life={playerLives[0]} onIncrement={() => incrementLife(0)} onDecrement={() => decrementLife(0)} />
+  </div>
+  <Counter life={playerLives[1]} onIncrement={() => incrementLife(1)} onDecrement={() => decrementLife(1)} />
 </main>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  type CounterProps = {
+    life: number
+    onIncrement: () => void
+    onDecrement: () => void
+  }
+  const { life, onIncrement, onDecrement }: CounterProps = $props()
+</script>
+
+<div class="relative flex bg-green-800">
+  <div class="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center text-9xl">
+    {life}
+  </div>
+</div>
