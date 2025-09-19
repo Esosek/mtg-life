d02558b34fb96d254a74163b1c4d5d8c16ce5064
Aleš Zima <zima.ales@seznam.cz>
2025-09-19 14:23:46 +0200

    added 5 more avatars

--- src/lib/avatars.ts
+++ src/lib/avatars.ts
@@ -3,6 +3,11 @@ import chandraAvatar from '../assets/chandra.jpg'
 import ajaniAvatar from '../assets/ajani.jpg'
 import lilianaAvatar from '../assets/liliana.jpg'
 import garrukAvatar from '../assets/garruk.jpg'
+import yajaAvatar from '../assets/yaja.jpg'
+import nivmizzetAvatar from '../assets/nivmizzet.jpg'
+import nissaAvatar from '../assets/nissa.jpg'
+import sarkhanAvatar from '../assets/sarkhan.jpg'
+import teferiAvatar from '../assets/teferi.jpg'
 
 const Avatars = {
   Jace: jaceAvatar,
@@ -10,6 +15,11 @@ const Avatars = {
   Ajani: ajaniAvatar,
   Liliana: lilianaAvatar,
   Garruk: garrukAvatar,
+  Yaja: yajaAvatar,
+  'Niv-Mizzet': nivmizzetAvatar,
+  Nissa: nissaAvatar,
+  Sarkhan: sarkhanAvatar,
+  Teferi: teferiAvatar,
 }
 
 export default Avatars
